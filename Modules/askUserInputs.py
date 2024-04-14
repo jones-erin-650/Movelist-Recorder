401011dabec0fd9e941a101915c2b0a2b1156cc5
Stephen Alfano <stephen.alfano@outlook.com>
2024-04-14 01:46:37 -0400

    added input functions

---
+++ Modules/askUserInputs.py
@@ -0,0 +1,18 @@
+def inputCharacter():
+    character = input("Enter a character: ")
+    return character
+def inputInputFile():
+    file = input("Enter input file: ")
+    return file
+def inputOutputFile():
+    file = input("Enter output file: ")
+    return file
+def inputStartRecHotkey():
+    key = input("Enter hotkey for starting recording: ")
+    return key
+def inputEndRecHotkey():
+    key = input("Enter hotkey for ending recording: ")
+    return key
+def inputTrainingReset():
+    key = input("Enter the key you will use to reset the training room: ")
+    return key
