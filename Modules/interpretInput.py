1a1baf2f2c34fa39fbd9d967a59c5d7691922050
eribrin <joneserin650@gmail.com>
2024-04-17 18:54:27 -0400

    a

--- Modules/interpretInput.py
+++ Modules/interpretInput.py
@@ -1,1 +1,13 @@
-# this should take in a shortcut or input name, ie. dew, mnt, etc, and output the corresponding input from the json file
+# this should take in an input name, ie. dew, mnt, qcf, etc, and output the corresponding input from the json file
+# also takes in the character name because some characters have inputs with the same name but different inputs, like how multiple characters have a backturn stance with different ways to access it
+def interpretInput(characterName, inputName, jsonFile):
+    # open json file
+
+    # open the character name object in json
+
+    # open the corresponding input
+
+    # output the input string
+    output = ""
+
+    return output
