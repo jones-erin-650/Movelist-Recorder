8fb188a2fcea7dd6ffe5119f2b45e0a195b0e2ef
BuildTools <unconfigured@null.spigotmc.org>
2024-04-18 22:58:22 -0400

    finished making recording filepath prompt in gui, and created a json file to keep track of important data (like the recording filepath)

--- View/gui_main.py
+++ View/gui_main.py
@@ -1,21 +1,42 @@
 import tkinter as tk
 import tkinter.filedialog as fd
+import json
+
+with open('View/vars.json') as f:
+    data = json.load(f)
 
 #Create main window and set attributes
 window = tk.Tk()
 window.title('Test GUI')
 window.geometry('540x320')
+window.eval('tk::PlaceWindow . center')
 
 def filepath_prompt():
     promptbox = tk.Toplevel(window)
-    promptbox.title('Enter Filepath')
-    promptbox.geometry('500x150')
+    promptbox.title('Set Recording Filepath')
+    window.eval(f'tk::PlaceWindow {str(promptbox)} center')
+    promptbox.transient(window)
+    promptbox.grab_set()
+    # promptbox.geometry('425x90')
 
     ent = tk.Entry(promptbox, width=50)
-    ent.place(relx=0.5,rely=0.5,anchor='center')
-    filename = fd.askdirectory()
-    ent.insert(0,filename)
+    ent.grid(row=1,column=1)
+    if data['filepath_recording'] != 'unset':
+        ent.insert(0, data['filepath_recording'])
 
+    browse = tk.Button(promptbox, text='Browse', command=lambda: get_filepath(ent))
+    browse.grid(row=1, column=2)
+
+    confirm = tk.Button(promptbox, text='Confirm', command=promptbox.destroy)
+    confirm.grid(row=2,column=2)
+
+def get_filepath(entrybox):
+    filepath = fd.askdirectory()
+    entrybox.delete(0, 'end')
+    data['filepath_recording'] = filepath
+    with open('View/vars.json', 'w') as f:
+        json.dump(data, f)
+    entrybox.insert(0,filepath)
 
 #Create menu bar
 menubar = tk.Menu(window)
