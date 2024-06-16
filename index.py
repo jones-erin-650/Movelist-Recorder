fdc9c3dd0de082f8b13a17c01408c7a4f9766fc4
joneserin650 <joneserin650@gmail.com>
2024-06-16 15:42:27 -0400

    deleted files from the previous version

---
+++ index.py
@@ -0,0 +1,38 @@
+import Modules
+import os
+
+def main():
+    
+    # first we need to ask for the input files
+    inputFile = Modules.inputInputFile()
+    outputPath = Modules.inputOutputPath()
+    characterName = Modules.inputCharacterName()
+    outputFileName = Modules.inputOutputFileName()
+
+    # need to make a new output file that we will write to
+    outputFile = os.path.join(outputPath, outputFileName)
+
+    with open(outputFile, 'w') as fp:
+        print('created file', outputFile)
+
+
+
+    # a loop to go line by line through the input file and interpret what is written, then output to the output an eddie-input readable translation to the output file 
+    with open(inputFile) as file:
+        for line in file:
+            # loop body
+            # open the output file
+            f = open(outputFile, "a")
+            
+            f.write("Hello!")
+
+            f.write("Hello!")
+
+
+            # close output file
+            f.close
+            
+
+
+if __name__=="__main__": 
+    main() 
