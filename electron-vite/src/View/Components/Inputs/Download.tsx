3457d37b76d7cf5d983084b94833e3d4df377d95
joneserin650 <joneserin650@gmail.com>
2024-06-21 23:36:42 -0400

    made a download button component, and refactored the files tab

---
+++ electron-vite/src/View/Components/Inputs/Download.tsx
@@ -0,0 +1,13 @@
+// need to add functionality to disable it and make it load
+const Download = () => {
+  return (
+    <>
+      <button className="btn btn-primary">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"  d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
+        Download
+      </button>
+    </>
+  )
+}
+
+export default Download
