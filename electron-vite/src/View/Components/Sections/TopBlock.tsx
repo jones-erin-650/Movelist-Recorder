9e87e65166c7faef542a201a006b525eee0d0b4d
Stephen Alfano <stephen.alfano@outlook.com>
2024-06-22 00:43:43 -0400

    fixed top block layout

--- electron-vite/src/View/Components/Sections/TopBlock.tsx
+++ electron-vite/src/View/Components/Sections/TopBlock.tsx
@@ -4,11 +4,13 @@ import TekkenDropdown from '../Inputs/Dropdowns/TekkenDropdown'
 const TopBlock = () => {
   return (
     <div className="flex flex-col w-full border-opacity-50 py-2">
-      <div className="grid h-auto card bg-base-300 rounded-box place-items-center">
+      <div className="grid grid-cols-2 h-auto card bg-base-300 rounded-box place-items-center pb-4">
         <FileInput
           label='Input movelist text file'
         />
-        <TekkenDropdown />
+        <div className="mt-9">
+          <TekkenDropdown />
+        </div>
       </div>
     </div>
   )
