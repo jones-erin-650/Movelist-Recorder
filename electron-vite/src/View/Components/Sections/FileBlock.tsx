37c3c1543571f5558c9dead0c54db8cf837a959a
eribrin <joneserin650@gmail.com>
2024-06-23 21:09:02 -0400

    refactored file page

---
+++ electron-vite/src/View/Components/Sections/FileBlock.tsx
@@ -0,0 +1,34 @@
+import Download from "../Inputs/Download"
+import SoftwareDropdown from "../Inputs/Dropdowns/SoftwareDropdown"
+import Toggle from "../Inputs/Toggle"
+
+const FileBlock = () => {
+  return (
+    <>
+      <div className="divider divider-primary">File Parsing</div>
+      <div className="flex w-full align-content: center">
+        
+        {/* left side with file handling */}
+        <div className="h-auto w-2/6 card bg-base-300 rounded-box place-items-left p-4">
+
+          <SoftwareDropdown />
+
+          <br />
+
+          <Toggle text='Comment out errors?' />
+
+          <br />
+
+          <Download />
+            
+        </div>
+        <div className="divider divider-horizontal divider-primary"></div>
+        <div className="grid h-auto w-full  card bg-base-300 rounded-box place-items-center"></div>
+        
+      </div>
+
+    </>
+  )
+}
+
+export default FileBlock
--- electron-vite/src/View/Pages/Files.tsx
+++ electron-vite/src/View/Pages/Files.tsx
@@ -1,40 +1,12 @@
-import FileInput from "../Components/Inputs/FileInput"
-import TekkenDropdown from "../Components/Inputs/Dropdowns/TekkenDropdown"
 import TopBlock from "../Components/Sections/TopBlock"
-import SoftwareDropdown from "../Components/Inputs/Dropdowns/SoftwareDropdown"
-import Toggle from "../Components/Inputs/Toggle"
-import Download from "../Components/Inputs/Download"
+import FileBlock from "../Components/Sections/FileBlock"
 
 const Files = () => {
   return (
     <>
       <TopBlock />
 
-      <div className="divider divider-primary">File Parsing</div>
-
-
-
-      <div className="flex w-full align-content: center">
-        
-        {/* left side with file handling */}
-        <div className="h-auto w-2/6 card bg-base-300 rounded-box place-items-left p-4">
-
-          <SoftwareDropdown />
-
-          <br />
-
-          <Toggle text='Comment out errors?' />
-
-          <br />
-
-          <Download />
-            
-        </div>
-        <div className="divider divider-horizontal divider-primary"></div>
-        <div className="grid h-auto w-full  card bg-base-300 rounded-box place-items-center"></div>
-        
-      </div>
-
+      <FileBlock />
 
 
     </>
