9d4685ad8f48ff86d53ad581226c53339275e7a7
Stephen Alfano <stephen.alfano@outlook.com>
2024-06-23 22:24:41 -0400

    started building recording page

---
+++ electron-vite/src/View/Components/Recording/Stream.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+const Stream = () => {
+  const [display, setSelection] = useState();
+
+  
+  
+  return (
+    <video>Stream</video>
+  )
+}
+
+export default Stream
--- electron-vite/src/View/Pages/Recording.tsx
+++ electron-vite/src/View/Pages/Recording.tsx
@@ -1,9 +1,25 @@
 import TopBlock from "../Components/Sections/TopBlock"
+import Stream from "../Components/Recording/Stream"
+import BasicDropdown from "../Components/Inputs/Dropdowns/BasicDropdown"
+
+const { desktopCapturer } = require('electron');
+
+async function getDisplays() {
+  const displays = await desktopCapturer.getSources({
+    types: ['window', 'screen']
+  });
+}
 
 const Recording = () => {
   return (
     <>
+      <script src="https://requirejs.org/docs/release/2.3.5/minified/require.js"></script>
       <TopBlock />
+      <Stream />
+      {/* <BasicDropdown 
+        text="Select display to be captured"
+        characters={}
+      /> */}
     </>
   )
 }
