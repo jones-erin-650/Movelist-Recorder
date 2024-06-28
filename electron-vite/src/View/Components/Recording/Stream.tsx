1153b242d580a684e9c8d7e0bc5066b39cc7f0af
BuildTools <unconfigured@null.spigotmc.org>
2024-06-27 20:55:07 -0400

    testing Stream component functionality

--- electron-vite/src/View/Components/Recording/Stream.tsx
+++ electron-vite/src/View/Components/Recording/Stream.tsx
@@ -1,38 +1,12 @@
-//i don't think this will work because electron doesn't allow getDisplayMedia. might have to fix the require() thing and use desktopCapturer
 
-const startRecording = () => {
-  console.log("recording");
-  navigator.mediaDevices.getDisplayMedia({
-    audio: true,
-    video: {
-      height: 700,
-      width: 1200
-    }
-  })
-  .then((stream) => {
-    const recorder = new MediaRecorder(stream);
-    recorder.start();
-    const buffer = [];
-    recorder.addEventListener('dataavailable', (event) => {
-      // @ts-ignore
-      buffer.push(event.data) 
-    })
-    recorder.addEventListener('stop', () => {
-      const blob = new Blob(buffer, {
-        type: 'video/mp4'
-      })
-
-      const video = document.getElementById('stream');
-      // @ts-ignore
-      video.href = URL.createObjectURL(blob);
-    })
-  })
-} 
+function getSources() {
+	
+}	
 
 const Stream = () => {
   return (
     <div className="flex justify-center">
-      <button onClick={startRecording}></button>
+      <button className="bg-red-500 w-12" onClick={getSources}></button>
       <video id="stream" controls className=""></video>
     </div>
     )
