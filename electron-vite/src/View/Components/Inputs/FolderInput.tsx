d3d87efa9d84f056ef441e67172b3588e36475eb
BuildTools <unconfigured@null.spigotmc.org>
2024-06-29 20:17:52 -0400

    worked on output directory button

--- electron-vite/src/View/Components/Inputs/FolderInput.tsx
+++ electron-vite/src/View/Components/Inputs/FolderInput.tsx
@@ -1,20 +1,28 @@
+import { useState } from "react";
+
 //need to fix this to prevent default file prompt from opening as well
-
-function openFolderDialog() {
-  //@ts-ignore
-  window.api.selectFolder();
-}
-
 const FolderInput = ({label}: {label: string}) => {
 
+  const [outputDirectory, setDirectory] = useState('');
+
+  function handleFolderSelect(event) {
+    event.preventDefault()
+    //@ts-ignore
+    const outputFolder = window.api.selectFolder().then(event => {return event})
+    console.log(outputFolder)
+    const input = document.getElementById('input')
+    if(outputFolder != undefined) {
+      input?.setAttribute('value', outputFolder)
+    }
+  }
 
   return (
-    <div onClick={openFolderDialog}>
+    <div onClick={handleFolderSelect}>
       <label className="form-control w-full max-w-xs px-2">
         <div className="label">
           <span className="label-text">{label}</span>
         </div>
-        <input type="file" className="file-input file-input-bordered w-full max-w-xs file-input-primary" />
+        <input type="file" id='input' className="file-input file-input-bordered w-full max-w-xs file-input-primary" onChange={handleFolderSelect}/>
       </label>
     </div>
   )
